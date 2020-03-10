5802feb3992ef24b8b915be2fd65e2743236ffdd
Hishaam De Vries <Hishaamdevries@gmail.com>
2020-03-10 15:31:28 +0200

    DataType exercise for Python

---
+++ DataTypes/DataTypes Ex2.py
@@ -0,0 +1,14 @@
+#This code prints the Numbers list
+Numbers = [56,78,34,21,56,34,125,45,89,75,12,56]
+print(Numbers)
+
+#This code finds the sum of all the numbers in the Numbers list
+x = sum(Numbers)
+print(x)
+
+#This code prints the smallest and highest numbers in the Numbers list
+print(min(Numbers), max(Numbers))
+
+#This code prints the Numbers list without any Duplicates
+a = list(set(Numbers))
+print(a)
